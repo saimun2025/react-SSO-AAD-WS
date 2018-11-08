b9616cc614f15bd17f3732d32706e46d3b250ab4
saimun <saimun2025@gmail.com>
2018-11-08 06:54:41 +0600

    action cable plagin added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import { runWithAdal } from 'react-adal';
 import { authContext } from './adalConfig';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 import * as serviceWorker from './serviceWorker';
 runWithAdal(authContext, () => {
     // TODO : continue your process
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(  <ActionCableProvider url={API_WS_ROOT}>
+    <App />
+  </ActionCableProvider>, document.getElementById('root'));
 
 });
 
